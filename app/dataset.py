42e9640e91a7b9733f3e32ae962f2d3450daae75
dowon <ks2515@naver.com>
2022-11-11 14:15:09 +0900

    feat: dataset

---
+++ app/dataset.py
@@ -0,0 +1,62 @@
+from typing import Optional
+
+import pytorch_lightning as pl
+from datasets import Dataset as HFDataset
+from datasets import load_dataset
+from torch.utils.data import DataLoader, Dataset
+from transformers import DataCollatorForLanguageModeling, PreTrainedTokenizerFast
+
+
+class TextDataset(Dataset):
+    def __init__(self, dataset: HFDataset, tokenizer: PreTrainedTokenizerFast):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int):
+        item = self.dataset[idx]
+        text = item["text"]
+        inputs = self.tokenizer(
+            text, truncation=True, return_tensors="pt", return_special_tokens_mask=True
+        )
+        for k, v in inputs.items():
+            inputs[k] = v.squeeze(0)
+        return inputs
+
+
+class TextDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        tokenizer: PreTrainedTokenizerFast,
+        batch_size: int = 32,
+        num_workers: int = 12,
+    ):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.collator = DataCollatorForLanguageModeling(tokenizer)
+
+    def prepare_data(self) -> None:
+        load_dataset(
+            "Bingsu/my-korean-training-corpus",
+            split="train",
+            use_auth_token=True,
+        )
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        dataset = load_dataset(
+            "Bingsu/my-korean-training-corpus", split="train", use_auth_token=True
+        )
+        self.train_dataset = TextDataset(dataset, self.tokenizer)
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            collate_fn=self.collator,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
